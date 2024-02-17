476cc0b726a825dd431e5dc1275324593c857a0d
alperentan <tanalperen8@gmail.com>
2024-02-18 02:41:28 +0300

    route changes for gh pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { NavBar, Favorites, SearchBody } from "./components";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<SearchBody />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<SearchBody />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
